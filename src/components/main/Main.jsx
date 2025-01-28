35a93398fb5fd5c9e132a163179444601a93b3ce
Arielle Farhi <ariel.farhi@stu.shenkar.ac.il>
2025-01-28 17:08:07 +0200

    Making the search bar work

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../card/Card";
 import carsData from "../../data/carsData";
 import "./main.css";
 
-function Main() {
+function Main({ showFavorites, searchQuery, filters }) {
+  const [favorites, setFavorites] = useState([]);
+
+  const toggleFavorite = (car) => {
+    setFavorites((prevFavorites) =>
+      prevFavorites.some((fav) => fav.id === car.id)
+        ? prevFavorites.filter((fav) => fav.id !== car.id)
+        : [...prevFavorites, car]
+    );
+  };
+
+  const filteredCars = carsData.filter((car) => {
+    const matchSearch = car.name
+      .toLowerCase()
+      .startsWith(searchQuery.toLowerCase());
+    const matchType =
+      filters.types.length === 0 || filters.types.includes(car.type);
+    const matchCapacity =
+      filters.capacities.length === 0 ||
+      filters.capacities.includes(car.capacity);
+    const matchPrice = car.pricePerDay <= filters.maxPrice;
+
+    return matchSearch && matchType && matchCapacity && matchPrice;
+  });
+
+  const carsToDisplay = showFavorites
+    ? favorites.filter((car) => filteredCars.includes(car))
+    : filteredCars;
+
   return (
     <main>
       <div className="catalogue-header">
         <h1 className="catalogue-title">Cars Catalogue</h1>
-        <span className="catalogue-count">{carsData.length} Cars</span>
+        <span className="catalogue-count">{carsToDisplay.length} Cars</span>
       </div>
       <div className="cards-container">
-        {carsData.map((car, index) => (
-          <Card key={index} car={car} />
+        {carsToDisplay.map((car) => (
+          <Card
+            key={car.id}
+            car={car}
+            toggleFavorite={() => toggleFavorite(car)}
+          />
         ))}
       </div>
     </main>
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import './searchBar.css'; 
 
-function SearchBar() {
+function SearchBar({ onSearch }) {
+  const handleInputChange = (e) => {
+    onSearch(e.target.value); 
+  };
+
   return (
     <div className="search-bar">
       <input
         type="text"
         className="search-input"
         placeholder="Search by car name"
+        onChange={handleInputChange} 
       />
       <SearchIcon className="search-icon" />
     </div>
