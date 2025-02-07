55aa8f34bfc530977eb4ad17145d530bea83b008
Arielle Farhi <ariel.farhi@stu.shenkar.ac.il>
2025-02-07 12:35:24 +0200

    Remove comments

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useAppContext } from "../AppContext"; // שימוש בקונטקסט
+// import { useAppContext } from "../AppContext"; 
 import logo from "../../Assets/Logo.png";
 import "./logo.css";
 
 const Logo = () => {
   const navigate = useNavigate();
-  const { setFilters } = useAppContext();
+  // const { setFilters } = useAppContext();
 
   const handleClick = (e) => {
     e.preventDefault();
-    navigate("/"); // חזרה לעמוד הבית מבלי לאפס סטייטים
+    navigate("/"); 
   };
 
   return (
